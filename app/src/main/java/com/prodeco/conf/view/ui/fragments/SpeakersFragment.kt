ded983ba1bb97ebb3c1595945f4f04334d22b5e5
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-11 23:52:57 -0500

    Inplementando carga de datos desde el ViewModel SpeakersFragment

--- app/src/main/java/com/prodeco/conf/view/ui/fragments/SpeakersFragment.kt
+++ app/src/main/java/com/prodeco/conf/view/ui/fragments/SpeakersFragment.kt
@@ -5,56 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.prodeco.conf.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.prodeco.conf.model.Speaker
+import com.prodeco.conf.view.adapter.SpeakerAdapter
+import com.prodeco.conf.view.adapter.SpeakerListener
+import com.prodeco.conf.viewmodel.SpeakersViewModel
+import kotlinx.android.synthetic.main.fragment_speakers.*
 
 /**
  * A simple [Fragment] subclass.
  * Use the [SpeakersFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SpeakersFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SpeakersFragment : Fragment(), SpeakerListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var speakerAdapter: SpeakerAdapter
+    private lateinit var speakerViewModel: SpeakersViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_speakers, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SpeakersFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SpeakersFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        speakerViewModel = ViewModelProviders.of(this).get(SpeakersViewModel::class.java)
+        speakerViewModel.refresh()
+
+        speakerAdapter = SpeakerAdapter(this)
+
+        rvSpeakers.apply {
+            layoutManager = GridLayoutManager(context,2)
+            adapter= speakerAdapter
+        }
+        observeViewModel()
+    }
+
+    fun observeViewModel(){
+        speakerViewModel.listSpeakers.observe(this, Observer<List<Speaker>> { speakers ->
+            speakers.let {
+                speakerAdapter.updateData(speakers)
             }
+        })
+
+        speakerViewModel.isLoading.observe(this, Observer<Boolean> {
+            if(it != null)
+                rlBaseSpeakers.visibility = View.INVISIBLE
+        })
+    }
+
+    override fun onSpeakerClicked(speaker: Speaker, position: Int) {
+        val bundle = bundleOf("speaker" to speaker)
+        findNavController().navigate(R.id.speakerDetailFragmentDialog, bundle)
+
     }
 }

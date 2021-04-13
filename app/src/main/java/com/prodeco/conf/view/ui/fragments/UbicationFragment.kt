29c3ba9fa9e7ae2ab4491e04e15ff7a07cc6f77b
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-13 00:20:46 -0500

    Añadir y personalizar un marcador en el mapa

--- app/src/main/java/com/prodeco/conf/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/prodeco/conf/view/ui/fragments/UbicationFragment.kt
@@ -1,18 +1,21 @@
 package com.prodeco.conf.view.ui.fragments
 
 import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.prodeco.conf.R
 import com.prodeco.conf.model.Ubication
@@ -40,6 +43,27 @@ class UbicationFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
+        val ubication = Ubication()
+
+        //Manipular el mapa
+        val zoom = 16f
+        val centerMap = LatLng(ubication.latitude, ubication.longitude)
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap, zoom))
+
+        //Personalizar pin google maps
+        val centerMark = LatLng(ubication.latitude, ubication.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(centerMark)
+        markerOptions.title("Platzi Conf 2019")
+
+        val bitmapDraw = context?.applicationContext?.let { ContextCompat.getDrawable(it, R.drawable.logo_platzi) } as BitmapDrawable
+        val smallMarker = Bitmap.createScaledBitmap(bitmapDraw.bitmap, 100, 100, false)
+
+        //Agregar al mapa
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+        googleMap?.addMarker(markerOptions)
 
     }
+
 }
